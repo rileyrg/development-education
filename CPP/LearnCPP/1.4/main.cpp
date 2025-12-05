205291a7010524aab73db63ec145353920d15066
rileyrg <rileyrg@gmx.de>
2025-12-06 00:56:51 +0100

    1.4.2

--- CPP/LearnCPP/1.4/main.cpp
+++ CPP/LearnCPP/1.4/main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 
 int main() {
-  int width; // define a variable named width
-  width = 5; // copy assignment of value 5 into variable width
-
+  int width{5}; // define variable width and initialize with initial value 5
   std::cout << width; // prints 5
-
-  width = 7; // change value stored in variable width to 7
-
-  std::cout << width; // prints 7
 
   return 0;
 }
