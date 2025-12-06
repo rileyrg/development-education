fa76786b9270d1765fbb32709ff935a1c52ac825
rileyrg <rileyrg@gmx.de>
2025-12-06 23:00:46 +0100

    1.6-implementation defined

--- CPP/LearnCPP/1.6/main.cpp
+++ CPP/LearnCPP/1.6/main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 
 int main() {
-  // define an integer variable named x
-  int x; // this variable is uninitialized because we haven't given it a value
-
-  // print the value of x to the screen
-  std::cout << x
-            << '\n'; // who knows what we'll get, because x is uninitialized
+  std::cout << sizeof(int)
+            << '\n'; // print how many bytes of memory an int value takes
 
   return 0;
 }
