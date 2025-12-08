4c686cc6488b84ed2507060d1a05f017b6c07618
rileyrg <rileyrg@gmx.de>
2025-12-08 07:27:34 +0100

    cin fixed

--- CPP/LearnCPP/1.11/main.cpp
+++ CPP/LearnCPP/1.11/main.cpp
@@ -5,7 +5,7 @@ int main() // note: this program has an error somewhere
   std::cout << "Enter an integer: ";
 
   int num{};       // define variable num as an integer variable
-  std::cin << num; // get integer value from user's keyboard
+  std::cin >> num; // get integer value from user's keyboard
 
   return 0;
 }
