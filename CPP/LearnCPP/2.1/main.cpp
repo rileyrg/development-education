53c1f94df4b9c54d8a48f71a52cb839e9ab645e6
rileyrg <rileyrg@gmx.de>
2025-12-08 07:42:42 +0100

    functions calling functions

--- CPP/LearnCPP/2.1/main.cpp
+++ CPP/LearnCPP/2.1/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream> // for std::cout
 
-// Definition of user-defined function doPrint()
-// doPrint() is the called function in this example
-void doPrint() { std::cout << "In doPrint()\n"; }
+void doB() { std::cout << "In doB()\n"; }
 
-// Definition of user-defined function main()
+void doA() {
+  std::cout << "Starting doA()\n";
+
+  doB();
+
+  std::cout << "Ending doA()\n";
+}
+
+// Definition of function main()
 int main() {
   std::cout << "Starting main()\n";
-  doPrint(); // Interrupt main() by making a function call to doPrint().  main()
-             // is the caller.
-  std::cout
-      << "Ending main()\n"; // This statement is executed after doPrint() ends
+
+  doA();
+
+  std::cout << "Ending main()\n";
 
   return 0;
 }
