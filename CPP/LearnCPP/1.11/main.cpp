5b98c1914e734bd874ee4b382ab4059115095581
rileyrg <rileyrg@gmx.de>
2025-12-08 07:29:41 +0100

    best solution

--- CPP/LearnCPP/1.11/main.cpp
+++ CPP/LearnCPP/1.11/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-int main() // note: this program has an error somewhere
-{
+// preferred version
+int main() {
   std::cout << "Enter an integer: ";
 
-  int num{};       // define variable num as an integer variable
-  std::cin >> num; // get integer value from user's keyboard
+  int num{};
+  std::cin >> num;
+
+  std::cout << "Double that number is: " << num * 2
+            << '\n'; // use an expression to multiply num * 2 at the point where
+                     // we are going to print it
 
   return 0;
 }
