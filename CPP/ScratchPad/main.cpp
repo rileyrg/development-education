17482e7d1589a60a02c67698cc501f25e0ba83f0
rileyrg <rileyrg@gmx.de>
2025-12-18 20:04:17 +0100

    aha mpw the range works. silly me.

--- CPP/ScratchPad/main.cpp
+++ CPP/ScratchPad/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 int main() {
-    std::vector<int> values = {1, 2, 3, 4, 5};
-    auto placeholder = 99;
-    values.push_back(placeholder);
-    for (int i=0; i < values.size();i++) {
-        std::cout << values[i] << "\n";
+    std::vector<int> values = {11, 12, 13, 14, 15};
+    values.push_back(99);
+    for (auto i:values) {
+    // for (int i=0; i<values.size();i++) {
+        std::cout << i << "\n";
     }
     return 0;
 }
