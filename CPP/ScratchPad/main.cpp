a33ac4698bc10fdd1da449f355f1a24d7f5c88f1
rileyrg <rileyrg@gmx.de>
2025-12-18 19:50:39 +0100

    still core dump

--- CPP/ScratchPad/main.cpp
+++ CPP/ScratchPad/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
-
 int main() {
-    std::vector<int> values{1, 2, 3, 4, 5};
-    for (auto i : values) {
+    std::vector<int> values;
+    values = std::vector<int>{1, 2, 3, 4, 5};
+
+    auto placeholder = 99;
+    values.push_back(placeholder);
+    for (int i : values) {
         std::cout << values[i] << std::endl;
     }
-    std::cin.get();
+    return 0;
 }
