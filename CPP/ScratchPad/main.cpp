c1ee608b025deab2627db79a7a931ebd662d21b9
rileyrg <rileyrg@gmx.de>
2025-12-19 21:44:29 +0100

    playing with clangd config

--- CPP/ScratchPad/main.cpp
+++ CPP/ScratchPad/main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <array>
 
 void foo(int x)
 {
+
     int buf[10];
     buf[x] = 0;
     auto placeholder = x == 1000;
     if (placeholder) {
     }
 
-    char a[10];
-    a[10] = 0;
+    std::array<int,3>  arr={1,2,3};
+    arr[1] = 0;
 
-    int *p;
-    p = new int[10];
+    int const* p = new const int[3]{1, 2, 3};
+    
+    //delete p;
 }
+    
 
-int main() {
+auto main() -> int {
     std::vector<int> values = {11, 12, 13, 14, 15};
     values.push_back(99);
     for (const auto&  i:values) {
