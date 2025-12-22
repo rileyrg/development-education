1577c1bbbeeabb58f1e78e72acca276ba63613bf
rileyrg <rileyrg@gmx.de>
2025-12-22 13:25:39 +0100

    2.9 not working in emacs project

--- CPP/ScratchPad/main.cpp
+++ CPP/ScratchPad/main.cpp
@@ -11,14 +11,16 @@ void foo(int x)
     if (placeholder) {
     }
 
+    int yy;
+    
     std::array<int,3>  arr={1,2,3};
     arr[1] = 0;
-
-    int const* p = new const int[3]{1, 2, 3};
-    
-    //delete p;
+    int const* p = new const int[3] {
+        1,
+        2,
+        3};
+    //delete [] p;
 }
-    
 
 auto main() -> int {
     std::vector<int> values = {11, 12, 13, 14, 15};
