e3ec5f9835a6a4a11917019c733d40d71975259c
rileyrg <rileyrg@gmx.de>
2026-01-13 22:12:39 +0100

    .dir-locals.el

--- CPP/LearnCPP/helloworld/.dir-locals.el
+++ CPP/LearnCPP/helloworld/.dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "cmake --build build.debug --clean-first --target main  && ./build.debug/main"))))
+((nil . ((compile-command . "cmake --build build.debug --clean-first --target HelloWorld  && ./build.debug/HelloWorld"))))
