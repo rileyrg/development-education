5dd31a43fd88c526d10b19b810f48d6c510730ae
rileyrg <rileyrg@gmx.de>
2026-01-20 10:07:29 +0100

    learncpp 4.10

---
+++ CPP/LearnCPP/4.10/src/main.cpp
@@ -0,0 +1,21 @@
+// https://www.learncpp.com/cpp-tutorial/introduction-to-if-statements/
+
+
+#include <iostream>
+
+// returns the absolute value of x
+int abs(int x)
+{
+    if (x < 0)
+        return -x; // early return (only when x < 0)
+
+    return x;
+}
+
+int main()
+{
+    std::cout << abs(4) << '\n'; // prints 4
+    std::cout << abs(-3) << '\n'; // prints 3
+
+    return 0;
+}
