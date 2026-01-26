04a4468a2dce7c836c21d03f39bae44e5bb8a641
rileyrg <rileyrg@gmx.de>
2026-01-26 17:45:33 +0100

    5.5 constant expressions Examples of constant and non-constant expressions

---
+++ CPP/LearnCPP/5.5/src/main.cpp
@@ -0,0 +1,42 @@
+// https://www.learncpp.com/cpp-tutorial/constant-expressions/
+
+#include <iostream>
+
+int getNumber()
+{
+    std::cout << "Enter a number: ";
+    int y{};
+    std::cin >> y; // can only execute at runtime
+
+    return y;      // this return expression is a runtime expression
+}
+
+// The return value of a non-constexpr function is a runtime expression
+// even when the return expression is a constant expression
+int five()
+{
+    return 5;      // this return expression is a constant expression
+}
+
+int main()
+{
+    // Literals can be used in constant expressions
+    5;                           // constant expression
+    1.2;                         // constant expression
+    "Hello world!";              // constant expression
+
+    // Most operators that have constant expression operands can be used in constant expressions
+    5 + 6;                       // constant expression
+    1.2 * 3.4;                   // constant expression
+    8 - 5.6;                     // constant expression (even though operands have different types)
+    sizeof(int) + 1;             // constant expression (sizeof can be determined at compile-time)
+
+    // The return values of non-constexpr functions can only be used in runtime expressions
+    getNumber();                 // runtime expression
+    five();                      // runtime expression (even though the return expression is a constant expression)
+
+    // Operators without constant expression operands can only be used in runtime expressions
+    std::cout << 5;              // runtime expression (std::cout isn't a constant expression operand)
+
+    return 0;
+}
