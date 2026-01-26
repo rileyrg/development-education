d5d00a8e82671e5303e473ee09bf3ceef8b862c4
rileyrg <rileyrg@gmx.de>
2026-01-26 17:32:44 +0100

    c++20 format library

--- CPP/LearnCPP/5.3/src/main.cpp
+++ CPP/LearnCPP/5.3/src/main.cpp
@@ -1,14 +1,14 @@
 // https://www.learncpp.com/cpp-tutorial/numeral-systems-decimal-binary-hexadecimal-and-octal/
 #include <format> // C++20
 #include <iostream>
-#include <print> // C++23
+// #include <print> // C++23
 
 int main()
 {
     std::cout << std::format("{:b}\n", 0b1010);  // C++20, {:b} formats the argument as binary digits
     std::cout << std::format("{:#b}\n", 0b1010); // C++20, {:#b} formats the argument as 0b-prefixed binary digits
 
-    std::println("{:b} {:#b}", 0b1010, 0b1010);  // C++23, format/print two arguments (same as above) and a newline
+//    std::println("{:b} {:#b}", 0b1010, 0b1010);  // C++23, format/print two arguments (same as above) and a newline
 
     return 0;
 }
