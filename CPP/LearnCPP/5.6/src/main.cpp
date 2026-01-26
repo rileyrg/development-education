1460fe56ebbd1c370dbf8b49a2c762474273444b
rileyrg <rileyrg@gmx.de>
2026-01-26 17:48:54 +0100

    5.6 constexpr with errors

---
+++ CPP/LearnCPP/5.6/src/main.cpp
@@ -0,0 +1,25 @@
+// https://www.learncpp.com/cpp-tutorial/constexpr-variables/
+#include <iostream>
+
+// The return value of a non-constexpr function is not constexpr
+int five()
+{
+    return 5;
+}
+
+int main()
+{
+    constexpr double gravity { 9.8 }; // ok: 9.8 is a constant expression
+    constexpr int sum { 4 + 5 };      // ok: 4 + 5 is a constant expression
+    constexpr int something { sum };  // ok: sum is a constant expression
+
+    std::cout << "Enter your age: ";
+    int age{};
+    std::cin >> age;
+
+    constexpr int myAge { age };      // compile error: age is not a constant expression
+    constexpr int f { five() };       // compile error: return value of five() is not constexpr
+
+    return 0;
+}
+
