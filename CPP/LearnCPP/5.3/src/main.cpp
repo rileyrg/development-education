57ef3ffd4cfd801327462df5603e4299a5404265
rileyrg <rileyrg@gmx.de>
2026-01-26 17:25:25 +0100

    c++14 binary literals

--- CPP/LearnCPP/5.3/src/main.cpp
+++ CPP/LearnCPP/5.3/src/main.cpp
@@ -3,7 +3,11 @@
 
 int main()
 {
-    int x{ 012 }; // 0 before the number means this is octal
-    std::cout << x << '\n';
+    int bin{};        // assume 16-bit ints
+    bin = 0b1;        // assign binary 0000 0000 0000 0001 to the variable
+    bin = 0b11;       // assign binary 0000 0000 0000 0011 to the variable
+    bin = 0b1010;     // assign binary 0000 0000 0000 1010 to the variable
+    bin = 0b11110000; // assign binary 0000 0000 1111 0000 to the variable
+
     return 0;
 }
