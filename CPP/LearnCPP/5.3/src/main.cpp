7248d719a750c830e2f2c02900ed01a92deb334b
rileyrg <rileyrg@gmx.de>
2026-01-26 17:29:20 +0100

    output in binaty

--- CPP/LearnCPP/5.3/src/main.cpp
+++ CPP/LearnCPP/5.3/src/main.cpp
@@ -3,15 +3,17 @@
 
 #include <iostream>
 
+#include <bitset> // for std::bitset
+#include <iostream>
+
 int main()
 {
-    int x { 12 };
-    std::cout << x << '\n'; // decimal (by default)
-    std::cout << std::hex << x << '\n'; // hexadecimal
-    std::cout << x << '\n'; // now hexadecimal
-    std::cout << std::oct << x << '\n'; // octal
-    std::cout << std::dec << x << '\n'; // return to decimal
-    std::cout << x << '\n'; // decimal
+	// std::bitset<8> means we want to store 8 bits
+	std::bitset<8> bin1{ 0b1100'0101 }; // binary literal for binary 1100 0101
+	std::bitset<8> bin2{ 0xC5 }; // hexadecimal literal for binary 1100 0101
 
-    return 0;
+	std::cout << bin1 << '\n' << bin2 << '\n';
+	std::cout << std::bitset<4>{ 0b1010 } << '\n'; // create a temporary std::bitset and print it
+
+	return 0;
 }
