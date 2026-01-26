ac12b53c3528d648fcbc515dd1014832cc47a5c5
rileyrg <rileyrg@gmx.de>
2026-01-26 17:05:33 +0100

    const parameters

--- CPP/LearnCPP/5.1/src/main.cpp
+++ CPP/LearnCPP/5.1/src/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 
+void printInt(const int x)
+{
+    std::cout << x << '\n';
+}
+
 int main()
 {
-    std::cout << "Enter your age: ";
-    int age{};
-    std::cin >> age;
-
-    const int constAge { age }; // initialize const variable using non-const value
-
-    age = 5;      // ok: age is non-const, so we can change its value
-    constAge = 6; // error: constAge is const, so we cannot change its value
+    printInt(5); // 5 will be used as the initializer for x
+    printInt(6); // 6 will be used as the initializer for x
 
     return 0;
 }
