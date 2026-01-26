5687c2d5140469b828b3be94e5976bbbf4757dad
rileyrg <rileyrg@gmx.de>
2026-01-26 17:23:59 +0100

    number bases octal 5.3

---
+++ CPP/LearnCPP/5.3/src/main.cpp
@@ -0,0 +1,9 @@
+// https://www.learncpp.com/cpp-tutorial/numeral-systems-decimal-binary-hexadecimal-and-octal/
+#include <iostream>
+
+int main()
+{
+    int x{ 012 }; // 0 before the number means this is octal
+    std::cout << x << '\n';
+    return 0;
+}
