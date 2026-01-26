9176057f8768cf06c9ee46fff4ef6e6bd8ad8164
rileyrg <rileyrg@gmx.de>
2026-01-26 17:12:21 +0100

    float and double literals

--- CPP/LearnCPP/5.2/src/main.cpp
+++ CPP/LearnCPP/5.2/src/main.cpp
@@ -1,13 +1,9 @@
-// https:  // www.learncpp.com/cpp-tutorial/literals/
-
-
 #include <iostream>
 
 int main()
 {
-    std::cout << 5 << '\n';  // 5 (no suffix) is type int (by default)
-    std::cout << 5L << '\n'; // 5L is type long
-    std::cout << 5u << '\n'; // 5u is type unsigned int
+    std::cout << 5.0 << '\n';  // 5.0 (no suffix) is type double (by default)
+    std::cout << 5.0f << '\n'; // 5.0f is type float
 
     return 0;
 }
