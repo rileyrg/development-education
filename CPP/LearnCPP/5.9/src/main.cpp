6d39f6babc1ddda4f2dd1df2c6a75b775d22f3a2
rileyrg <rileyrg@gmx.de>
2026-01-27 15:50:42 +0100

    revalidating an invalidated string_view

--- CPP/LearnCPP/5.9/src/main.cpp
+++ CPP/LearnCPP/5.9/src/main.cpp
@@ -4,30 +4,23 @@
 #include <string>
 #include <string_view>
 
-void printSV(std::string_view str) // now a std::string_view, creates a view of the argument
-{
-    std::cout << str << '\n';
-}
 
-// std::string_view takes a different approach to initialization. Instead of making an expensive copy of the initialization string, std::string_view creates an inexpensive view of the initialization string. The std::string_view can then be used whenever access to the string is required.
-
-// In the context of our analogy, std::string_view is a viewer. It views an object that already exists elsewhere, and cannot modify that object. When the view is destroyed, the object being viewed is not affected. Having multiple viewers viewing an object simultaneously is fine.
-
-// It is important to note that a std::string_view remains dependent on the initializer through its lifetime. If the string being viewed is modified or destroyed while the view is still being used, unexpected or undefined behavior will result.
-
-// Whenever we use a view, it is up to us to ensure these possibilities
-// do not occur.
+// Invalidated objects can often be revalidated (made valid again) by
+// setting them back to a known good state. For an invalidated
+// std::string_view, we can do this by assigning the invalidated
+// std::string_view object a valid string to view.
 
 
 int main()
 {
-    printSV("Hello, world!"); // call with C-style string literal
+    std::string s { "Hello, world!" };
+    std::string_view sv { s }; // sv is now viewing s
 
-    std::string s2{ "Hello, world!" };
-    printSV(s2); // call with std::string
+    s = "Hello, universe!";    // modifies s, which invalidates sv (s is still valid)
+    std::cout << sv << '\n';   // undefined behavior
 
-    std::string_view s3 { s2 };
-    printSV(s3); // call with std::string_view
+    sv = s;                    // revalidate sv: sv is now viewing s again
+    std::cout << sv << '\n';   // prints "Hello, universe!"
 
     return 0;
 }
