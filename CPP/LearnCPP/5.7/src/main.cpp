4baac587ca723ccfd3594968c07471bb3aceef08
rileyrg <rileyrg@gmx.de>
2026-01-27 13:03:41 +0100

    5.7 stdstring

---
+++ CPP/LearnCPP/5.7/src/main.cpp
@@ -0,0 +1,10 @@
+// https://www.learncpp.com/cpp-tutorial/introduction-to-stdstring/
+#include <string> // allows use of std::string
+
+int main()
+{
+    std::string name {}; // empty string
+
+    return 0;
+}
+
