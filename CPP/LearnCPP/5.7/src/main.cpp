9ad2c1aede76df2b8d03066ca950d93ef6652731
rileyrg <rileyrg@gmx.de>
2026-01-27 15:38:16 +0100

    constexpr std::string_view

--- CPP/LearnCPP/5.7/src/main.cpp
+++ CPP/LearnCPP/5.7/src/main.cpp
@@ -1,14 +1,12 @@
 // https://www.learncpp.com/cpp-tutorial/introduction-to-stdstring/
+
 #include <iostream>
-#include <string>
+#include <string_view>
 
 int main()
 {
-    using namespace std::string_literals;
-
-    constexpr std::string name{ "Alex"s }; // compile error
-
-    std::cout << "My name is: " << name;
+    constexpr std::string_view s{ "Hello, world!" }; // s is a string symbolic constant
+    std::cout << s << '\n'; // s will be replaced with "Hello, world!" at compile-time
 
     return 0;
 }
---
+++ CPP/LearnCPP/5.8/src/main.cpp
@@ -0,0 +1,20 @@
+// 5.8 — Introduction to std::string_view https://www.learncpp.com/cpp-tutorial/introduction-to-stdstring_view/
+
+#include <iostream>
+#include <string>
+#include <string_view>
+
+int main()
+{
+    std::string_view s1 { "Hello, world!" }; // initialize with C-style string literal
+    std::cout << s1 << '\n';
+
+    std::string s{ "Hello, world!" };
+    std::string_view s2 { s };  // initialize with std::string
+    std::cout << s2 << '\n';
+
+    std::string_view s3 { s2 }; // initialize with std::string_view
+    std::cout << s3 << '\n';
+
+    return 0;
+}
