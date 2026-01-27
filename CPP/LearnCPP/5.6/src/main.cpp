14a5d83c7b44a1d1c585f52bb810e6973d34f011
rileyrg <rileyrg@gmx.de>
2026-01-27 13:02:06 +0100

    more constexpr good and bad

--- CPP/LearnCPP/5.6/src/main.cpp
+++ CPP/LearnCPP/5.6/src/main.cpp
@@ -1,25 +1,31 @@
 // https://www.learncpp.com/cpp-tutorial/constexpr-variables/
 #include <iostream>
 
-// The return value of a non-constexpr function is not constexpr
-int five()
+int max(int x, int y) // this is a non-constexpr function
 {
-    return 5;
+    if (x > y)
+        return x;
+    else
+        return y;
+}
+
+constexpr int cmax(int x, int y) // this is a constexpr function
+{
+    if (x > y)
+        return x;
+    else
+        return y;
 }
 
 int main()
 {
-    constexpr double gravity { 9.8 }; // ok: 9.8 is a constant expression
-    constexpr int sum { 4 + 5 };      // ok: 4 + 5 is a constant expression
-    constexpr int something { sum };  // ok: sum is a constant expression
+    int m1 { max(5, 6) };            // ok
+    const int m2 { max(5, 6) };      // ok
+    constexpr int m3 { max(5, 6) };  // compile error: max(5, 6) not a constant expression
 
-    std::cout << "Enter your age: ";
-    int age{};
-    std::cin >> age;
-
-    constexpr int myAge { age };      // compile error: age is not a constant expression
-    constexpr int f { five() };       // compile error: return value of five() is not constexpr
+    int m4 { cmax(5, 6) };           // ok: may evaluate at compile-time or runtime
+    const int m5 { cmax(5, 6) };     // ok: may evaluate at compile-time or runtime
+    constexpr int m6 { cmax(5, 6) }; // okay: must evaluate at compile-time
 
     return 0;
 }
-
