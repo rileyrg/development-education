2a8f51e06145228f8fcbd3f2c972ed8ec0356239
rileyrg <rileyrg@gmx.de>
2026-01-27 13:07:47 +0100

    std::cin >> std::ws

--- CPP/LearnCPP/5.7/src/main.cpp
+++ CPP/LearnCPP/5.7/src/main.cpp
@@ -1,9 +1,18 @@
 // https://www.learncpp.com/cpp-tutorial/introduction-to-stdstring/
-#include <string> // allows use of std::string
+#include <iostream>
+#include <string>
 
 int main()
 {
-    std::string name {}; // empty string
+    std::cout << "Pick 1 or 2: ";
+    int choice{};
+    std::cin >> choice;
+
+    std::cout << "Now enter your name: ";
+    std::string name{};
+    std::getline(std::cin >> std::ws, name); // note: added std::ws here
+
+    std::cout << "Hello, " << name << ", you picked " << choice << '\n';
 
     return 0;
 }
