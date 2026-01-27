1fe3d93e071ec5a3de95a0b1fd225bb5f92b7b22
rileyrg <rileyrg@gmx.de>
2026-01-27 20:10:38 +0100

    introduction of project specific history (emacs project)

--- CPP/LearnCPP/6.1/src/main.cpp
+++ CPP/LearnCPP/6.1/src/main.cpp
@@ -16,17 +16,14 @@ void printCalculation(int x, int y, int z)
     std::cout << x + (y * z);
 }
 
-// If you run this program and enter the inputs 1, 2, and 3, you might
-// assume that this program would calculate 1 + (2 * 3) and print 7. But
-// that is making the assumption that the arguments to printCalculation()
-// will evaluate in left-to-right order (so parameter x gets value 1, y
-// gets value 2, and z gets value 3). If instead, the arguments evaluate
-// in right-to-left order (so parameter z gets value 1, y gets value 2,
-//                         and x gets value 3), then the program will print 5 instead.
-
 int main()
 {
-    printCalculation(getValue(), getValue(), getValue()); // this line is ambiguous
+    // can be made unambiguous by making each function call to getValue() a separate statement:
+    int a{ getValue() }; // will execute first
+    int b{ getValue() }; // will execute second
+    int c{ getValue() }; // will execute third
+
+    printCalculation(a, b, c); // this line is now unambiguous
 
     return 0;
 }
