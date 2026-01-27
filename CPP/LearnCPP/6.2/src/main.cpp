fd010f295bba200d3a1be676a75ebaf4cb3d23f9
rileyrg <rileyrg@gmx.de>
2026-01-27 20:21:32 +0100

    divide by zero

--- CPP/LearnCPP/6.2/src/main.cpp
+++ CPP/LearnCPP/6.2/src/main.cpp
@@ -4,14 +4,12 @@
 
 int main()
 {
-    constexpr int x{ 7 };
-    constexpr int y{ 4 };
+	constexpr int apples { 12 };
+	std::cout << "You have " << apples << " apples. Enter how many people to divide them between: ";
+	int x {};
+	std::cin >> x;
 
-    std::cout << "int / int = " << x / y << '\n';
-    std::cout << "double / int = " << static_cast<double>(x) / y << '\n';
-    std::cout << "int / double = " << x / static_cast<double>(y) << '\n';
-    std::cout << "double / double = " << static_cast<double>(x) / static_cast<double>(y) << '\n';
+	std::cout << "Each person gets " << apples / x << " whole apples.\n"; // apples and x are int, so this is integer division
 
-    return 0;
+	return 0;
 }
-
