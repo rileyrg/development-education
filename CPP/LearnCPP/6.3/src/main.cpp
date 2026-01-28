b8319bd7a27a65fb2afa425b930640f3fb45017e
rileyrg <rileyrg@gmx.de>
2026-01-28 02:42:15 +0100

    tinkering

--- CPP/LearnCPP/6.3/src/main.cpp
+++ CPP/LearnCPP/6.3/src/main.cpp
@@ -1,39 +1,38 @@
-// 6.3 — Remainder and Exponentiation https://www.learncpp.com/cpp-tutorial/remainder-and-exponentiation/
-#include <cassert> // for assert
-#include <cstdint> // for std::int64_t
+// 6.3 — Remainder and Exponentiation
+// https://www.learncpp.com/cpp-tutorial/remainder-and-exponentiation/
+
+#include <cassert>  // for assert
+#include <cstdint>  // for std::int64_t
 #include <iostream>
-#include <limits> // for std::numeric_limits
+#include <limits>  // for std::numeric_limits
 
 // A safer (but slower) version of powint() that checks for overflow
 // note: exp must be non-negative
 // Returns std::numeric_limits<std::int64_t>::max() if overflow occurs
-constexpr std::int64_t powint_safe(std::int64_t base, int exp)
-{
+constexpr std::int64_t powint_safe(std::int64_t base, int exp) {
     assert(exp >= 0 && "powint_safe: exp parameter has negative value");
 
     // Handle 0 case
-    if (base == 0)
-        return (exp == 0) ? 1 : 0;
+    if (base == 0) {
+        return (exp == 0);
+    }
 
-    std::int64_t result { 1 };
+    std::int64_t result{1};
 
     // To make the range checks easier, we'll ensure base is positive
     // We'll flip the result at the end if needed
-    bool negativeResult{ false };
+    bool negativeResult{false};
 
-    if (base < 0)
-    {
+    if (base < 0) {
         base = -base;
-        negativeResult = (exp & 1);
+        negativeResult = ((exp & 1) != 0);
     }
 
-    while (exp > 0)
-    {
-        if (exp & 1) // if exp is odd
+    while (exp > 0) {
+        if (exp & 1)  // if exp is odd
         {
             // Check if result will overflow when multiplied by base
-            if (result > std::numeric_limits<std::int64_t>::max() / base)
-            {
+            if (result > std::numeric_limits<std::int64_t>::max() / base) {
                 std::cerr << "powint_safe(): result overflowed\n";
                 return std::numeric_limits<std::int64_t>::max();
             }
@@ -44,14 +43,14 @@ constexpr std::int64_t powint_safe(std::int64_t base, int exp)
         exp /= 2;
 
         // If we're done, get out here
-        if (exp <= 0)
+        if (exp <= 0) {
             break;
+        }
 
         // The following only needs to execute if we're going to iterate again
 
         // Check if base will overflow when multiplied by base
-        if (base > std::numeric_limits<std::int64_t>::max() / base)
-        {
+        if (base > std::numeric_limits<std::int64_t>::max() / base) {
             std::cerr << "powint_safe(): base overflowed\n";
             return std::numeric_limits<std::int64_t>::max();
         }
@@ -59,16 +58,18 @@ constexpr std::int64_t powint_safe(std::int64_t base, int exp)
         base *= base;
     }
 
-    if (negativeResult)
+    if (negativeResult) {
         return -result;
+    }
 
     return result;
 }
 
-int main()
-{
-	std::cout << powint_safe(7, 12) << '\n'; // 7 to the 12th power
-	std::cout << powint_safe(70, 12) << '\n'; // 70 to the 12th power (will return the max 64-bit int value)
+int main() {
+    std::cout << powint_safe(7, 12) << '\n';  // 7 to the 12th power
+    std::cout
+        << powint_safe(70, 12)
+        << '\n';  // 70 to the 12th power (will return the max 64-bit int value)
 
-	return 0;
+    return 0;
 }
