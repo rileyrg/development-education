ce67d0da28e4b8dda5837ef4fe8a5eb4a51a20af
rileyrg <rileyrg@gmx.de>
2026-02-04 14:48:37 +0100

    7.3 local variables

---
+++ CPP/LearnCPP/7.3/src/main.cpp
@@ -0,0 +1,21 @@
+// 7.3 — local variables 
+// https://www.learncpp.com/cpp-tutorial/local-variables
+#include <iostream>
+
+int main()
+{
+    int y { 5 }; // we're declaring y here because we need it in this outer block later
+
+    {
+        int x{};
+        std::cin >> x;
+
+        // if we declared y here, immediately before its actual first use...
+        if (x == 4)
+            y = 4;
+    } // ... it would be destroyed here
+
+    std::cout << y; // and we need y to exist here
+
+    return 0;
+}
