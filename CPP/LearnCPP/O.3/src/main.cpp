10f1fa6fb0b5847b65b8ca3952efafa030b308e5
rileyrg <rileyrg@gmx.de>
2026-02-04 13:10:00 +0100

    O.3 bit manipulation defining masks c++14

---
+++ CPP/LearnCPP/O.3/src/main.cpp
@@ -0,0 +1,14 @@
+// O.3 — Bit manipulation with bitwise operators and bit masks
+// https://www.learncpp.com/cpp-tutorial/bit-manipulation-with-bitwise-operators-and-bit-masks/
+
+#include <cstdint>
+
+constexpr std::uint8_t mask0{ 0b0000'0001 }; // represents bit 0
+constexpr std::uint8_t mask1{ 0b0000'0010 }; // represents bit 1
+constexpr std::uint8_t mask2{ 0b0000'0100 }; // represents bit 2
+constexpr std::uint8_t mask3{ 0b0000'1000 }; // represents bit 3
+constexpr std::uint8_t mask4{ 0b0001'0000 }; // represents bit 4
+constexpr std::uint8_t mask5{ 0b0010'0000 }; // represents bit 5
+constexpr std::uint8_t mask6{ 0b0100'0000 }; // represents bit 6
+constexpr std::uint8_t mask7{0b1000'0000};   // represents bit 7
+
