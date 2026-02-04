0d5d6de01711909edc0598b51aea6c97890cd9b0
rileyrg <rileyrg@gmx.de>
2026-02-04 16:07:16 +0100

    quick summary

--- CPP/LearnCPP/7.7/src/main.cpp
+++ CPP/LearnCPP/7.7/src/main.cpp
@@ -1,14 +1,25 @@
 // 7.7 — External linkage and variable forward declarations
 // https://www.learncpp.com/cpp-tutorial/external-linkage-and-variable-forward-declarations/
 
-#include <iostream>
 
+// quick summary
+// 
 extern int g_x;       // this extern is a forward declaration of a variable named g_x that is defined somewhere else
-extern const int g_y; // this extern is a forward declaration of a const variable named g_y that is defined somewhere else
+extern const int g_y;  // this extern is a forward declaration of a
+                       // const variable named g_y that is defined
+// somewhere else
 
-int main()
-{
-    std::cout << g_x << ' ' << g_y << '\n'; // prints 2 3
+// // Global variable forward declarations (extern w/ no initializer):
+// extern int g_y;                 // forward declaration for non-constant global variable
+// extern const int g_y;           // forward declaration for const global variable
+// extern constexpr int g_y;       // not allowed: constexpr variables can't be forward declared
 
-    return 0;
-}
+// // External global variable definitions (no extern)
+// int g_x;                        // defines non-initialized external global variable (zero initialized by default)
+// int g_x { 1 };                  // defines initialized external global variable
+
+// // External const global variable definitions (extern w/ initializer)
+
+// extern const int g_x { 2 };     // defines initialized const external global
+// variable extern constexpr int g_x{3};    // defines initialized constexpr
+// // external global variable
