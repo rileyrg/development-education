6fa52862eb8f3488fbf9a142f7c7d9c9dbf51634
rileyrg <rileyrg@gmx.de>
2026-02-04 15:48:42 +0100

    Global variables with internal linkage

---
+++ CPP/LearnCPP/7.6/src/main.cpp
@@ -0,0 +1,14 @@
+// 7.6 — Internal linkage
+// https://www.learncpp.com/cpp-tutorial/internal-linkage/
+#include <iostream>
+
+static int g_x{}; // non-constant globals have external linkage by default, but can be given internal linkage via the static keyword
+
+const int g_y{ 1 }; // const globals have internal linkage by default
+constexpr int g_z{ 2 }; // constexpr globals have internal linkage by default
+
+int main()
+{
+    std::cout << g_x << ' ' << g_y << ' ' << g_z << '\n';
+    return 0;
+}
