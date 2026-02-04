ed6d5098fad2b4dc6aa8932c447262bd2338d564
rileyrg <rileyrg@gmx.de>
2026-02-04 14:11:06 +0100

    Using the scope resolution operator with no name prefix

--- CPP/LearnCPP/7.2/src/main.cpp
+++ CPP/LearnCPP/7.2/src/main.cpp
@@ -2,27 +2,23 @@
 // https://www.learncpp.com/cpp-tutorial/user-defined-namespaces-and-the-scope-resolution-operator/
 #include <iostream>
 
-namespace Foo // define a namespace named Foo
+void print() // this print() lives in the global namespace
 {
-    // This doSomething() belongs to namespace Foo
-    int doSomething(int x, int y)
-    {
-        return x + y;
-    }
+	std::cout << " there\n";
 }
 
-namespace Goo // define a namespace named Goo
+namespace Foo
 {
-    // This doSomething() belongs to namespace Goo
-    int doSomething(int x, int y)
-    {
-        return x - y;
-    }
+	void print() // this print() lives in the Foo namespace
+	{
+		std::cout << "Hello";
+	}
 }
 
 int main()
 {
-    std::cout << Foo::doSomething(4, 3) << '\n'; // use the doSomething() that exists in namespace Foo
-    std::cout << Goo::doSomething(4, 3) << '\n'; // use the doSomething() that exists in namespace Goo
-    return 0;
+	Foo::print(); // call print() in Foo namespace
+	::print();    // call print() in global namespace (same as just calling print() in this case)
+
+	return 0;
 }
